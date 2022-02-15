f2c5be93a8b862da8f446b3d16df06db6fadd8d3
narahari-anvita <79611794+narahari-anvita@users.noreply.github.com>
2022-02-14 17:16:53 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function music(){
+    window.location = "stand.html";
+}
+
+function tv(){
+    window.location = "tv.html";
+}
+
+function desk(){
+    window.location = "desk.html";
+}
+
+function hoverboard(){
+    window.location = "hoverboard.html";
+}
+
+function holder(){
+    window.location = "holder.html";
+}
